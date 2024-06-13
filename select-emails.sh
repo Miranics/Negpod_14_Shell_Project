6102a29594389a0c5ab8d8b807fbaa7e551c1609
Sharif2138 <169043842+Sharif2138@users.noreply.github.com>
2024-06-13 17:43:33 +0200

    Update select-emails.sh

--- select-emails.sh
+++ select-emails.sh
@@ -5,7 +5,7 @@ STUDENT_FILE="students-list_0524.txt"
 # File to save student emails
 EMAIL_FILE="student-emails.txt"
 
-# Check if the student file exists
+# Checking if the student file exists
 if [ -f "$STUDENT_FILE" ]; then
     # Extract emails and save to the email file
     cut -d',' -f2 "$STUDENT_FILE" > "$EMAIL_FILE"
